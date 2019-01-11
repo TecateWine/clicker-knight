2a8218144a5f19d8adc8944daed6694b94291217
TecateWine <tecatewine@protonmail.com>
2019-01-11 14:13:21 -0800

    Added heroku into settings

--- clickerknight/settings.py
+++ clickerknight/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clickerknight',
     'website',
+
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, '/static'),
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
