72f6ab883b00450350fe2fa94cdf1d342749675c
TecateWine <tecatewine@protonmail.com>
2019-01-11 14:15:12 -0800

    .

--- clickerknight/settings.py
+++ clickerknight/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clickerknight',
     'website',
+    'django-heroku',
 
 
 ]
